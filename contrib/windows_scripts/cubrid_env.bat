67112373344f465607e3d4557516524264070687
airnet73 <61675061+airnet73@users.noreply.github.com>
2021-03-30 14:38:41 +0900

    [CBRD-21446] Installing CUBRID with zip on Windows, cubrid server start fail. (#14)
    * [CBRD-21446] add copyright and license comment
    
    * [CBRD-21446] modify copyright and license format
    
    * [CBRD-21446] Change license to GPL2
    
    * [CBRD-21446] Add Foundation address to license

--- contrib/windows_scripts/cubrid_env.bat
+++ contrib/windows_scripts/cubrid_env.bat
@@ -1,4 +1,21 @@
 @echo off
+
+REM  Copyright 2016 CUBRID Corporation
+REM
+REM   This program is free software; you can redistribute it and/or modify
+REM   it under the terms of the GNU General Public License as published by
+REM   the Free Software Foundation; either version 2 of the License, or
+REM   (at your option) any later version.
+REM
+REM  This program is distributed in the hope that it will be useful,
+REM  but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+REM  GNU General Public License for more details.
+REM
+REM  You should have received a copy of the GNU General Public License
+REM  along with this program; if not, write to the Free Software
+REM  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
 rem batch script for CUBRID Environments, (window services, registry)
 
 rem LOADING CUBRID Environments
